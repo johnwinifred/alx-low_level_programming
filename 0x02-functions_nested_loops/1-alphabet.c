d586a8e89dcb74fd4a8909380f7a44a9c3a5c15e
johnwinifred <winifredjohnchidera123@gmail.com>
2023-03-23 18:31:12 -0700

    0-isupper.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,17 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
+
+void print_alphabet(void)
 {
-	char c = 'a'
-		while (c <= 'z'){
-			_putchar (c);
-			c++;
-		}
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	       	_putchar(c);
+		c++;
+
+	}
 	_putchar('\n');
-	return (0);
+
 }
